527d966d826c4fb44f64bdc2a5083d250791a697
PaienNate <2417971099@qq.com>
2024-08-29 13:40:28 +0800

    fix: 修正逻辑BUG引发的BUG

--- task/TaskResource.go
+++ task/TaskResource.go
@@ -3,20 +3,6 @@ package task
 // TaskResource 代表任务资源
 // 或许因为我们使用的ambari的DS，存储在HDFS上，导致这里通过ID根本无法获取任何信息
 // 应该手动定义点Resource
-type TaskResource []Resource
-
-type Resource struct {
+type TaskResource struct {
 	ResourceName string `json:"resourceName"`
-}
-
-// NewTaskResource 是一个构造函数，用于通过传入路径字符串生成 TaskResource
-// 路径字符串建议通过Resource相关函数去获得。
-func NewTaskResource(paths ...string) TaskResource {
-	var resources TaskResource
-	for _, path := range paths {
-		resources = append(resources, Resource{
-			ResourceName: path,
-		})
-	}
-	return resources
 }
