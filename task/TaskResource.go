c6004f615ff4a2e02e9e79d17eac6a2a316e9d22
PaienNate <2417971099@qq.com>
2024-08-29 10:28:43 +0800

    根据事实情况修改TaskResource

--- task/TaskResource.go
+++ task/TaskResource.go
@@ -1,27 +1,8 @@
 package task
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 // TaskResource 代表任务资源
+// 或许因为我们使用的ambari的DS，存储在HDFS上，导致这里通过ID根本无法获取任何信息
+// 取而代之的是一个resourceName []List
 type TaskResource struct {
-	ID int64 `json:"id"`
-}
-
-// NewTaskResource 创建一个新的 TaskResource 实例
-func NewTaskResource(id int64) *TaskResource {
-	return &TaskResource{
-		ID: id,
-	}
-}
-
-// ToString 将 TaskResource 对象转换为 JSON 字符串
-func (tr *TaskResource) ToString() (string, error) {
-	jsonData, err := json.Marshal(tr)
-	if err != nil {
-		return "", fmt.Errorf("error converting TaskResource to JSON string: %v", err)
-	}
-	return string(jsonData), nil
+	ResourceName []string `json:"resourceName"`
 }
